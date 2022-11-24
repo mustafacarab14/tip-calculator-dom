863458e87f0fba76c9373f4607863fae06f8386c
mustafe abdirahman <111310904+carabzl@users.noreply.github.com>
2022-11-24 18:58:15 +0300

    tip-calculator-dom

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello");
+document.querySelector("#btn").addEventListener("click",function(){
+    let mon =document.querySelector("#money").value
+    let bill = 275
+    let tip1 = bill * 0.15
+    let tip2 = bill * 0.20
+    let total_v1 = bill + tip1
+    let total_v2 = bill + tip2
+    if(bill >= 0 && bill <= 300){
+    document.querySelector("#result").innerHTML=`the bill was ${bill}$ the tip was ${tip1}$ and the total value is ${total_v1}$`
+    }else{
+    document.querySelector("#result").innerHTML=`the bill was ${bill}$ the tip was ${tip2}$ and the total value is ${total_v2}$`
+    }
+})
+
